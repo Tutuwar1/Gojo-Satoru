335b12e06ce9a47edf09cb93d2c7aba2d2293760
Tutuwar1 <107424990+Tutuwar1@users.noreply.github.com>
2022-06-13 22:29:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,12 +19,12 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['0763033021']
+global.premium = ['0763033021']
+global.ownernomer = '0763033021'
+global.ownername = '✔️TUTUWA'
+global.botname = '✔️TUTUWA'
+global.footer = '✔️TUTUWA.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
